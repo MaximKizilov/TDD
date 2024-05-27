49d1254fddecd850743e1b737d7df3850be1e6d2
“Maxim <kizilov2016@yandex.ru>
2024-05-27 22:02:08 +1000

    Stub and tests for add

--- src/main/java/ru/netology/PhoneBook.java
+++ src/main/java/ru/netology/PhoneBook.java
@@ -5,7 +5,9 @@ import java.util.Map;
 import java.util.TreeMap;
 
 public class PhoneBook {
-
+    public  void add(String key, String value){
+        return;
+    }
 
 
 
--- src/test/java/ru/netology/PhoneBookTest.java
+++ src/test/java/ru/netology/PhoneBookTest.java
@@ -6,7 +6,15 @@ import org.mockito.Mockito;
 import static org.junit.jupiter.api.Assertions.*;
 
 class PhoneBookTest {
+    void testadd(){
+        PhoneBook phoneBook = new PhoneBook();
+        phoneBook.add("Петя", " 8-914-177-46-05");
+        phoneBook.add("Петя", " 8-914-177-43-06");
+        phoneBook.add("Вася", " 8-914-177-46-05");
+        phoneBook.add("Вася", " 8-914-177-46-05");
 
+        assertEquals(phoneBook.count, 2);
+    }
 
 
 }
