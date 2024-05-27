5b92850522455444885870132c6ba28d7837c37f
“Maxim <kizilov2016@yandex.ru>
2024-05-27 22:00:24 +1000

    Initial commit

---
+++ src/main/java/ru/netology/PhoneBook.java
@@ -0,0 +1,12 @@
+package ru.netology;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.TreeMap;
+
+public class PhoneBook {
+
+
+
+
+}
---
+++ src/test/java/ru/netology/PhoneBookTest.java
@@ -0,0 +1,12 @@
+package ru.netology;
+
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PhoneBookTest {
+
+
+
+}
